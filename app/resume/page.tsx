56e50db2d503c0854e71f28aec5c7e64c14d64e7
Amal474 <mohammedamal.n@gmail.com>
2024-07-21 13:47:02 +0300

    [FIX] link issue - CV test 2

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,6 +2,6 @@
 export default function Resume() {
 
   return (
-    <iframe className="w-full min-h-[100vh]" src='Jabiring/assets/JABER SAIDALI NALAKATH_CV.pdf' />
+    <iframe className="w-full min-h-[100vh]" src="Jabiring/assets/CV.pdf" />
   );
 }
